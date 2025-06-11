3bec57690170ee2be6f1d70efab8992863747e68
Koiii-237 <koikou208@gmail.com>
2025-06-11 22:44:11 +0700

    Demo

--- src/SalesManagement/SalesManagement.java
+++ src/SalesManagement/SalesManagement.java
@@ -14,7 +14,8 @@ public class SalesManagement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("gay");
+        System.out.println("ngu");
+    }
     }
     
-}
